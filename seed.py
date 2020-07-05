137801f22965f9b99cabd1f1995017807025cc06
Nancy Yeh <heyycnan@gmail.com>
2020-07-04 20:30:21 -0700

    ready for deployment

---
+++ seed.py
@@ -0,0 +1,12 @@
+import os
+import crud
+import model
+import server
+
+os.system('dropdb chicken_tinder')
+os.system('createdb chicken_tinder')
+
+model.connect_to_db(server.app)
+model.db.create_all()
+
+crud.create_shortcodes()
